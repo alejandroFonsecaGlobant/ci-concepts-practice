55349a89b1f7dc62d26450d4d30c81e4ae401a75
Alejandro Fonseca Forero <alejandro.fonseca@globant.com>
2023-09-02 15:24:38 -0500

    Change program logic to auto print instead of deciding

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,17 +10,7 @@ public class App
 {
     public static void main( String[] args ) {
         WindowsEnvironment env = new WindowsEnvironment();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Select the variable you want to print:");
-        System.out.println("1. OS");
-        System.out.println("2. Computer name");
-        int decision = scanner.nextInt();
-        if (decision == 1) {
-            System.out.println(env.getOs());
-        } else if (decision == 2){
-            System.out.println(env.getComputerName());
-        } else {
-            System.out.println("Invalid number. Shutting down...");
-        }
+        System.out.println("Your OS is: " + env.getOs());
+        System.out.println("Your computer name is: " + env.getComputerName());
     }
 }
